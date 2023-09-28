4a24ee5cfb8893fccebf65e3fd9a47c35c66af83
NUGROHOADI11 <deeforce112002@gmail.com>
2023-09-28 10:29:05 +0700

    .

--- kegiatan_1.py
+++ kegiatan_1.py
@@ -1,3 +1,34 @@
+# Fungsi penjumlahan
+def add(x, y):
+    return x + y
+
+# Fungsi pengurangan
+def minus(x, y):
+    return x - y
+
+# Fungsi perkalian
+def mult(x, y):
+    return x * y
+
+# Fungsi pembagian
+def div(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Pembagian oleh nol tidak valid."
+
+# Contoh penggunaan fungsi-fungsi aritmatika
+result_add = add(3, 5)
+result_minus = minus(10, 4)
+result_mult = mult(6, 7)
+result_div = div(8, 2)
+
+print("Hasil penjumlahan:", result_add)
+print("Hasil pengurangan:", result_minus)
+print("Hasil perkalian:", result_mult)
+print("Hasil pembagian:", result_div)
+
+
 def tree(node):
     if isinstance(node, (int, float)):
         return node
